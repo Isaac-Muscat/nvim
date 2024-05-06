1946b43a525dfe37288bc817edb817905dfd05cb
Isaac Muscat <isaac.wieting.muscat@gmail.com>
2024-05-05 21:14:18 -0400

    Config has lsp and auto complete

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,11 +1,13 @@
 return {
-    "nvim-treesitter/nvim-treesitter", build = ":TSUpdate",
-    config = function()
-        local config = require("nvim-treesitter.configs")
-        config.setup({
-            ensure_installed = {"lua", "javascript", "c", "cpp", "rust"},
-            highlight = { enable = true },
-            indent = { enable = true },
-        })
-    end
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local config = require("nvim-treesitter.configs")
+		config.setup({
+			auto_install = true,
+			ensure_installed = { "lua", "javascript", "c", "cpp", "rust", "verilog" },
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
